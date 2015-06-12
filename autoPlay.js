94ecab588780f226877b7e53576a5d42e9a1c8c1
Eric Ahn <wchill1337@gmail.com>
2015-06-12 15:40:58 -0500

    Increment version number
    yeah I forget these things

--- autoPlay.js
+++ autoPlay.js
@@ -2,7 +2,7 @@
 // @name Monster Minigame Auto-script
 // @namespace https://github.com/wchill/steamSummerMinigame
 // @description A script that runs the Steam Monster Minigame for you. Modified from mouseas's original version to include autoclick.
-// @version 1.0
+// @version 1.01
 // @match http://steamcommunity.com/minigame/towerattack*
 // @updateURL https://raw.githubusercontent.com/wchill/steamSummerMinigame/master/autoPlay.js
 // @downloadURL https://raw.githubusercontent.com/wchill/steamSummerMinigame/master/autoPlay.js
