9dc037c18ca61562a68d72eb64521b0d4f6dd4da
Simone <chaufnet@gmail.com>
2015-06-13 17:50:25 +0200

    Bump version.

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -2,7 +2,7 @@
 // @name Monster Minigame Auto-script w/ auto-click
 // @namespace https://github.com/SteamDatabase/steamSummerMinigame
 // @description A script that runs the Steam Monster Minigame for you.
-// @version 2.9
+// @version 3.0
 // @match http://steamcommunity.com/minigame/towerattack*
 // @match http://steamcommunity.com//minigame/towerattack*
 // @grant none
