1a973a6ab25ec44f5a2e85de07d6fc46529fae54
Edward <oedw@users.noreply.github.com>
2015-06-13 23:57:07 -0700

    Fixed Switching to a raining gold lane
    lowTarget is always zero at that point, so the code will execute all the time. And besides, lowTarget would be the id of the enemy and not the type.

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -494,7 +494,7 @@ function goToLaneWithBestTarget() {
 		}
 
 		//Prefer lane with raining gold, unless current enemy target is a treasure or boss.
-		if(lowTarget != ENEMY_TYPE.TREASURE && lowTarget != ENEMY_TYPE.BOSS ){
+		if(!targetIsTreasureOrBoss){
 			var potential = 0;
 			// Loop through lanes by elemental preference
 			var sortedLanes = sortLanesByElementals();
