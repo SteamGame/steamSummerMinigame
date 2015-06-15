978bb8c34c5c99a6d4cc1682d299582a4f80ad7a
Fugitoid <Fugitoid@users.noreply.github.com>
2015-06-15 17:53:22 -0400

    Update autoPlay.user.js

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name [SteamDB] Monster Minigame Script
+// @name [SteamDB] Monster Minigame Script (Fugitoid fork) 
 // @namespace https://github.com/SteamDatabase/steamSummerMinigame
 // @description A script that runs the Steam Monster Minigame for you.
 // @version 4.4.5
 // @match *://steamcommunity.com/minigame/towerattack*
 // @match *://steamcommunity.com//minigame/towerattack*
 // @grant none
-// @updateURL https://raw.githubusercontent.com/SteamDatabase/steamSummerMinigame/master/autoPlay.user.js
-// @downloadURL https://raw.githubusercontent.com/SteamDatabase/steamSummerMinigame/master/autoPlay.user.js
+// @updateURL https://raw.githubusercontent.com/SteamDatabase/steamSummerMinigame/testing/autoPlay.user.js
+// @downloadURL https://raw.githubusercontent.com/SteamDatabase/steamSummerMinigame/testing/autoPlay.user.js
 // ==/UserScript==
 
 // IMPORTANT: Update the @version property above to a higher number such as 1.1 and 1.2 when you update the script! Otherwise, Tamper / Greasemonkey users will not update automatically.
