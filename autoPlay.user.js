b1fbb401d7eb48b4a410d1cd1faf8d829668319b
grimrebuke <grimrebuke@users.noreply.github.com>
2015-06-16 18:33:11 -0400

    Update autoPlay.user.js

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -2,7 +2,7 @@
 // @name [SteamDB] Monster Minigame Script
 // @namespace https://github.com/SteamDatabase/steamSummerMinigame
 // @description A script that runs the Steam Monster Minigame for you.
-// @version 4.6.9.1
+// @version 4.7.0
 // @match *://steamcommunity.com/minigame/towerattack*
 // @match *://steamcommunity.com//minigame/towerattack*
 // @grant none
