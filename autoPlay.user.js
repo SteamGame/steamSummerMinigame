f2c7845ff447e8120d0e60cabc04873aeeec20d0
Pavel <xPaw@users.noreply.github.com>
2015-06-18 18:27:13 +0300

    Change spam all abilities from 15 to 5 minutes at the end of the game

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -2,7 +2,7 @@
 // @name [SteamDB] Monster Minigame Script
 // @namespace https://github.com/SteamDatabase/steamSummerMinigame
 // @description A script that runs the Steam Monster Minigame for you.
-// @version 4.8.12
+// @version 4.9.0
 // @match *://steamcommunity.com/minigame/towerattack*
 // @match *://steamcommunity.com//minigame/towerattack*
 // @grant none
@@ -422,7 +422,7 @@ function MainLoop() {
 
 		var timeLeft = getTimeleft(); // Time left in minutes
 
-		if(timeLeft <= 15) {
+		if(timeLeft <= 5) {
 			useAllAbilities();
 		} else {
 			useAbilities(level);
