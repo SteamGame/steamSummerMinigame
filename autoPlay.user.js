e4681caaaef066b8182cae203d1faf7679f6b9b8
Simone <chaufnet@gmail.com>
2015-06-19 17:55:46 +0200

    Port changes.

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -426,7 +426,11 @@ function MainLoop() {
 	if (!isAlreadyRunning) {
 		isAlreadyRunning = true;
 
-		goToLaneWithBestTarget(level);
+if (level % control.rainingRounds === 0) {
+				goToRainingLane();
+			} else {
+				goToLaneWithBestTarget(level);
+			}
 
 		attemptRespawn();
 
@@ -1168,6 +1172,26 @@ function updatePlayersInGame() {
 	ELEMENTS.PlayersInGame.textContent = totalPlayers + "/1500";
 }
 
+function goToRainingLane() {
+	// On a WH level, jump everyone to lane 0, unless there is a boss there, in which case jump to lane 1.
+	var targetLane = 0;
+	// Check lane 0, enemy 0 to see if it's a boss.
+	var enemyData = s().GetEnemy(0, 0).m_data;
+	if (typeof enemyData !== "undefined") {
+		var enemyType = enemyData.type;
+		if (enemyType == ENEMY_TYPE.BOSS) {
+			advLog('In lane 0, there is a boss, avoiding', 4);
+			targetLane = 1;
+		}
+	}
+
+	if (s().m_nExpectedLane != targetLane) {
+		advLog('Switching to raining lane' + targetLane, 3);
+		s().TryChangeLane(targetLane);
+	}
+}
+
+
 function goToLaneWithBestTarget(level) {
 	// We can overlook spawners if all spawners are 40% hp or higher and a creep is under 10% hp
 	var spawnerOKThreshold = 0.4;
@@ -1360,6 +1384,9 @@ function useAbilities(level)
 	var enemySpawnerHealthPercent = false;
 	var enemy = false;
 	var enemyBossHealthPercent = 0;
+
+
+
 
 	// Medic & Pumped Up
 	if (tryUsingAbility(ABILITIES.PUMPED_UP)){
